551a8fc442791c4730c3de9a8fa30991660a5593
generation21 <qbration21@gmail.com>
2024-04-12 00:15:56 +0900

    lazy loader

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
-      "storage.googleapis.com",
-      "storage.googleapis.com/wedding-invitation-a4e03.appspot.com",
-    ],
-    dangerouslyAllowSVG: true,
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    loader: "custom",
+    loaderFile: "src/libs/myloader.ts",
+    // domains: [
+    //   "storage.googleapis.com",
+    //   "storage.googleapis.com/wedding-invitation-a4e03.appspot.com",
+    // ],
+    // dangerouslyAllowSVG: true,
+    // contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
 };
 
